30546a15eeb663d9689d5f71e547fd6730e415aa
Anand Patel <anandpatel3@hotmail.com>
2018-08-18 11:17:53 -0400

    comments

--- index.js
+++ index.js
@@ -11,38 +11,38 @@ app.use(cors(corsOptions));
 const Parrot = require('../api-parrot/index').Parrot(app, parrotOptions);
 
 // sourcesystem url scrubber middleware
-app.use(function(req, res, next) {
-  console.log(req.url);
-  if (req.url.indexOf('[') >= 0 && req.url.indexOf(']') >= 0) {
-    req.sourceSystemUrl = req.url.substring(
-      req.url.indexOf('[') + 1,
-      req.url.indexOf(']')
-    );
-    req.url = req.url.replace(/\[.*\]/g, '');
-  }
-  next();
-});
+// app.use(function(req, res, next) {
+//   console.log(req.url);
+//   if (req.url.indexOf('[') >= 0 && req.url.indexOf(']') >= 0) {
+//     req.sourceSystemUrl = req.url.substring(
+//       req.url.indexOf('[') + 1,
+//       req.url.indexOf(']')
+//     );
+//     req.url = req.url.replace(/\[.*\]/g, '');
+//   }
+//   next();
+// });
 // if scrubbed url is in parrot -> respond to it
 app.use(Parrot);
 
 // if bypass parrot -> go to source system
-app.use(function(req, res, next) {
-  console.log(req.sourceSystemUrl + req.url);
-  axios
-    .get(req.sourceSystemUrl + req.url, {
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-    .then(d => {
-      // console.log(d);
-      res.send(d.data);
-    })
-    .catch(err => {
-      // console.log(err);
-      res.send(err);
-    });
-});
+// app.use(function(req, res, next) {
+//   console.log(req.sourceSystemUrl + req.url);
+//   axios
+//     .get(req.sourceSystemUrl + req.url, {
+//       headers: {
+//         'Content-Type': 'application/json'
+//       }
+//     })
+//     .then(d => {
+//       // console.log(d);
+//       res.send(d.data);
+//     })
+//     .catch(err => {
+//       // console.log(err);
+//       res.send(err);
+//     });
+// });
 
 app.listen(4003, function() {
   console.log('listening on port 4003');
--- parrot-options.js
+++ parrot-options.js
@@ -6,13 +6,64 @@ module.exports = {
         id: 0123,
         firstName: 'Anand',
         lastName: 'Patel'
-      }
+      },
+      alias: 'User call 1'
     },
     {
       route: '/system',
       squack: {
+        'hello world':
+          'mock systemmock systemmock systemmock systemmock systemmock systemmock systemmock systemmock systemmock systemmock systemmock systemmock systemmock systemmock systemmock systemmock systemmock systemmock systemmock systemmock systemmock systemmock systemmock systemmock systemmock systemmock systemmock systemmock systemmock systemmock systemmock systemmock systemmock systemmock systemmock systemmock systemmock systemmock systemmock systemmock system'
+      },
+      alias: 'System call 1'
+    },
+    {
+      route: '/user2',
+      squack: {
+        id: 0123,
+        firstName: 'Anand',
+        lastName: 'Patel'
+      },
+      group: 'AVA'
+    },
+    {
+      route: '/system2',
+      squack: {
         'hello world': 'mock system'
-      }
+      },
+      group: 'AVA'
+    },
+    {
+      route: '/user3',
+      squack: {
+        id: 0123,
+        firstName: 'Anand',
+        lastName: 'Patel'
+      },
+      group: 'AVA'
+    },
+    {
+      route: '/system3',
+      squack: {
+        'hello world': 'mock system'
+      },
+      group: 'Glass'
+    },
+    {
+      route: '/user4',
+      squack: {
+        id: 0123,
+        firstName: 'Anand',
+        lastName: 'Patel'
+      },
+      group: 'Glass'
+    },
+    {
+      route: '/system4',
+      squack: {
+        'hello world': 'mock system'
+      },
+      group: 'Something'
     }
   ]
 };
